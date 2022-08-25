297938676a6caee95086d8ddbeb86146a0d8a7ae
Maicol Santos <maicolcostaa8@gmail.com>
2022-08-25 10:40:30 +0100

    Install storybook sctructure

---
+++ dist/index.es.js
@@ -0,0 +1,1 @@
+import e from"react";const r=()=>e.createElement("h1",null,"Hello Card");export{r as Card};
---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { Card } from '../components/Card'
+
+const stories = storiesOf('Card Test', module)
+
+stories.add('Card', () => {
+  return <Card />
+})
